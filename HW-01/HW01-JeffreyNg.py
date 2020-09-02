efb9c2fea74e4de2fc497a1bdbf7a30e063d7e76
komoto415 <jeffreyng99@gmail.com>
2020-09-02 13:13:59 -0500

    adding doc string and more handling with missing files

--- HW-01/HW01-JeffreyNg.py
+++ HW-01/HW01-JeffreyNg.py
@@ -8,10 +8,16 @@ UPPERCASE_ASCII_OFFSET: int = 65
 LOWERCASE_ASCII_OFFSET: int = 97
 CHAR_SET_SIZE: int = 26
 DEFAULT_USER_RESP: int = -1
+EXIT_CODE: int = 4
+STRINGIFY_LIST: Callable = "".join
 
-def shift_option(user_option: int) -> None:
+DEFAULT_PLAINTEXT_WRITE: str = "helloworld"
+DEFAULT_CIPHERTEXT_WRITE: str = "khoorzruog"
+
+
+def shift_option(user_resp: int) -> None:
     job_done: bool = False
-    option_name: str = ["encrypt", "decrypt"][user_option - 1]
+    option_name: str = ["encrypt", "decrypt"][user_resp - 1]
     while not job_done:
         try:
             n_shift: int = int(input(f"How much do you want to shift for your {option_name}ion? ").strip())
@@ -24,12 +30,19 @@ def shift_option(user_option: int) -> None:
 def encrypt(n: int = 0) -> None:
     with open(PLAINTEXT_PATH, "r") as plaintext_file:
         lines: List[str] = plaintext_file.readlines()
-        single_str: str = "".join(lines).replace('\n', '').replace(' ', '').strip()
-        shifted: str = "".join([shift(ch, n) for ch in single_str])
+        single_block_str: str = STRINGIFY_LIST(lines).replace('\n', '').replace(' ', '').strip()
+        shifted: str = STRINGIFY_LIST([shift(ch, n) for ch in single_block_str])
         with open(CIPHERTEXT_PATH, "w+") as ciphertext_file:
             ciphertext_file.write(shifted)
 
 def decrypt(n: int = 0) -> None:
+    """
+    Reads and decrypts ciphertext.txt and writes the decrypted message to plaintext.txt
+
+    :param n: the key shift
+    :type n: int
+    :return: None
+    """
     n *= -1
     with open(CIPHERTEXT_PATH, "r") as ciphertext_file:
         lines: List[str] = ciphertext_file.readlines()
@@ -37,21 +50,41 @@ def decrypt(n: int = 0) -> None:
             # plaintext_file.write('\n')
             # plaintext_file.write('\n')
             for i, line in enumerate(lines, 1):
-                new_line: str = "".join([shift(ch, n) for ch in line])
+                new_line: str = STRINGIFY_LIST([shift(ch, n) for ch in line])
                 plaintext_file.write(new_line)
+                # Just ensures file has no trailing new line
                 if i is not len(lines):
                     plaintext_file.write('\n')
 
 def break_cipher(_):
     print("Chose break_chipher")
 
-def shift(ch: str, n: int) -> chr:
+def shift(ch: chr, n: int) -> chr:
+    """
+    Shifts an english alphabetic  character n number times
+
+    :param ch: the character being shifted
+    :param n: the key shift
+    :type ch: chr
+    :type n: int
+    :return: the shifted character
+    :rtype: chr
+    :raises: TypeError: if the ch is not a character
+    """
+    try:
+        ord(ch)
+    except TypeError as e:
+        print(f"Error: {e}")
+        raise
+
     if not ch.isalpha():
         return ch
-    ch_as_ascii: int = ord(ch) - (UPPERCASE_ASCII_OFFSET if ch.isupper() else LOWERCASE_ASCII_OFFSET)
-    ch_as_ascii += n
-    ch_as_ascii %= CHAR_SET_SIZE
-    return chr(ch_as_ascii + (UPPERCASE_ASCII_OFFSET if ch.isupper() else LOWERCASE_ASCII_OFFSET))
+    # Offset ascii value to to 0-25 range
+    casing_offset: int = UPPERCASE_ASCII_OFFSET if ch.isupper() else LOWERCASE_ASCII_OFFSET
+    ch_zeroed_range: int = ord(ch) - casing_offset
+    ch_zeroed_range += n
+    ch_zeroed_range %= CHAR_SET_SIZE
+    return chr(ch_zeroed_range + casing_offset)
 
 MENU_FETCH: Dict[int, Callable] = {
     1: shift_option,
@@ -90,30 +123,40 @@ November 19, 1863
 
 def main():
     try:
-        with open(PLAINTEXT_PATH) as _:
-            pass
         with open(CIPHERTEXT_PATH) as _:
             pass
     except FileNotFoundError as e:
         print(f"Error: {e}")
-        raise
+        print(f"Creating {CIPHERTEXT_PATH} and writing in '{DEFAULT_CIPHERTEXT_WRITE}'")
+        with open(CIPHERTEXT_PATH, "w") as ciphertext_file:
+            ciphertext_file.write(DEFAULT_CIPHERTEXT_WRITE)
+            pass
 
-    user_option = DEFAULT_USER_RESP
-    while user_option is not 4:
+    try:
+        with open(PLAINTEXT_PATH) as _:
+            pass
+    except FileNotFoundError as e:
+        print(f"Error: {e}")
+        print(f"Creating {PLAINTEXT_PATH} and writing in '{DEFAULT_PLAINTEXT_WRITE}'")
+        with open(PLAINTEXT_PATH, "w") as plaintext_file:
+            plaintext_file.write(DEFAULT_PLAINTEXT_WRITE)
+
+    user_resp = DEFAULT_USER_RESP
+    while user_resp is not EXIT_CODE:
         print("""Welcome to my awesome encryption program
     1) Encrypt
     2) Decrypt
     3) Break
     4) Exit""")
         try:
-            user_option: int = int(input("Please pick an option: ").strip())
-            if user_option < 1 or user_option > 4:
+            user_resp: int = int(input("Please pick an option: ").strip())
+            if user_resp < 1 or user_resp > 4:
                 print(f"That is not a valid option\n")
                 continue
         except ValueError as e:
             print(f"Error: {e}\n")
         else:
-            MENU_FETCH.get(user_option)(user_option)
+            MENU_FETCH.get(user_resp)(user_resp)
         print()
 
 if __name__ == "__main__":
